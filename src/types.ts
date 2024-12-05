34d086ec1b22a948c0d443372268a43d3131adcd
mike-git-1 <mikee.tran67@gmail.com>
2024-12-05 16:59:38 -0500

    removed unused import

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,5 @@
 // properties that we expect to get back when we make a getMyUserRequest
 // export these typescritps types so that we can use them in our components and typescript doesnt get confused about what type 'User' is
-
-import { string } from "zod"
-
 // key note: mongo _id is of type ObjectId on the backend, but when it gets to the front end, its going to be a string
 export type User = {
   _id: string
